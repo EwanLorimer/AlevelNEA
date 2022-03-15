5275d961818f6af154082a031c73666e957e3dfc
EwanLorimer <ewan.lorimer@outlook,com>
2022-03-15 18:55:41 +0000

    Commit1

---
+++ scriptingFunctions.py
@@ -0,0 +1,18 @@
+class funcs:
+    
+    #Checks if account details entered by user at sign-up match the parameters (username has 6 or more characters, passwords are between 8 and 15 characters and match)
+    def checkSignUpParameters(username, password, passwordConfirm):
+        if len(username) >= 6:
+            if len(password) >= 8 and len(password) <= 15:
+                if password == passwordConfirm:
+                    return True
+                else:
+                    return False
+            else:
+                return False
+        else:
+            return False
+
+
+
+
